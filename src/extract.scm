d66c337af4ff4f8642578fd55d6a58b11fc9a747
Shiro Kawai <shiro@acm.org>
2002-01-14 09:21:20 +0000

    added
    git-svn-id: https://wiliki.svn.sourceforge.net/svnroot/wiliki/WiLiKi/trunk@28 38bef18e-7143-0410-826b-833c7993c044

---
+++ src/extract.scm
@@ -0,0 +1,64 @@
+;; extract translatable messages
+
+;; $Id: extract.scm,v 1.1 2002-01-14 09:21:20 shirok Exp $
+
+(define (scan-src file)
+  (define msgs '())
+  (define (rec expr)
+    (when (pair? expr)
+      (if (and (eq? (car expr) '$$)
+               (pair? (cdr expr))
+               (null? (cddr expr))
+               (string? (cadr expr)))
+          (push! msgs
+                 `(,(cadr expr) ,(pair-attribute-get expr 'source-info #f) ""))
+          (for-each rec expr))))
+  (rec (call-with-input-file file port->sexp-list))
+  msgs)
+
+(define (scan-msgfile file)
+  (if (file-exists? file)
+      (call-with-input-file file port->sexp-list)
+      '()))
+
+(define (main args)
+  (unless (= (length args) 2)
+    (error "usage: gosh extract.scm file.scm msg-file"))
+  (let* ((src-file (car args))
+         (dst-file (cadr args))
+         (dst-tmp  (string-append dst-file ".t"))
+         (src-msgs (scan-src src-file))
+         (dst-msgs (scan-msgfile dst-file)))
+    (for-each (lambda (dst-msg)
+                (cond ((assoc (car dst-msg) src-msgs)
+                       => (lambda (p)
+                            (set! (caddr p) (cadr dst-msg))
+                            (set! (cddr dst-msg) #t)))))
+              dst-msgs)
+    (call-with-output-file (string-append (cadr args) ".t")
+      (lambda (p)
+        (for-each (lambda (src-msg)
+                    (apply format p ";; ~a : line ~a\n" (cadr src-msg))
+                    (format p "(~s\n" (car src-msg))
+                    (format p " ~s\n" (caddr src-msg))
+                    (format p ")\n\n"))
+                  (reverse src-msgs))
+        (for-each (lambda (dst-msg)
+                    (when (null? (cddr dst-msg))
+                      (format p ";; obsoleted message\n")
+                      (format p ";; ~s\n" (car dst-msg))
+                      (format p ";; ~s\n\n" (cadr dst-msg))))
+                  (reverse dst-msgs))))
+    (when (file-exists? dst-file)
+      (sys-rename dst-file (string-append dst-file ".orig")))
+    (sys-rename dst-tmp dst-file))
+  0)
+
+
+                    
+
+
+
+
+
+
