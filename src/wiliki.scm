3d5dab3336c1b4d5dac08a8c9325e921dc9eb46b
Shiro Kawai <shiro@acm.org>
2010-04-25 19:51:38 +0000

    fix
    git-svn-id: https://wiliki.svn.sourceforge.net/svnroot/wiliki/WiLiKi/trunk@447 38bef18e-7143-0410-826b-833c7993c044

--- src/wiliki.scm
+++ src/wiliki.scm
@@ -385,11 +385,12 @@
          [w `(base (@ (href ,(wiliki:url :full))))]
          [w `(link (@ (rel "alternate") (type "application/rss+xml")
                       (title "RSS") (href ,(wiliki:url :full "c=rss"))))]
-         [(and w (ref w'style-sheet)) @
-          => (lambda (ss)
-               (map (lambda (s)
-                      `(link (@ (rel "stylesheet") (href ,s) (type "text/css"))))
-                    (if (list? s) s (list s))))])
+         [(and w (ref w'style-sheet))
+          => @(lambda (ss)
+                (map (lambda (s)
+                       `(link (@ (rel "stylesheet")
+                                 (href ,s) (type "text/css"))))
+                     (if (list? s) s (list s))))])
       )))
 
 (define (default-format-time time)
