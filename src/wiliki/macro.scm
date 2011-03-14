f4145cf76c86e30798f93ba1c6709649fb4485be
Shiro Kawai <shiro@acm.org>
2011-03-14 09:11:55 -1000

    Added ad-hoc comment spam filtering

--- src/wiliki/macro.scm
+++ src/wiliki/macro.scm
@@ -417,6 +417,7 @@
   (define (filter-suspicious content)
     (cond [(or (not (string? content))
                (#/<a\s+href=[\"'\s]*http/i content)
+               (#/\[url=http:\/\/[\w\/.-]*\]/i content)
                (#/^\s*comment\d*\s*$/i content)  ; temporary
                (#/^\s*[\d\;_.tx]+\s*$/ content)) ; temporary
            (wiliki:log-event "rejecting spam comment for ~s: name=~s content=~s"
