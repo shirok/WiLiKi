0b49e632fd72e95abff10e5e2587594cac59ed36
Shiro Kawai <shiro@acm.org>
2015-08-09 15:19:14 -1000

    Remove remanining hardcoded affiliate id

--- examples/blog/blog.scm
+++ examples/blog/blog.scm
@@ -629,17 +629,18 @@
 
 (define-reader-macro (amazon-affiliate asin . opts)
   (define aid (blog-option-ref (wiliki) 'amazon-affiliate-id))
+  (define aid-us (blog-option-ref (wiliki) 'amazon-affiliate-id-us))
   (let-macro-keywords* opts ([domain "jp"][float "left"])
     (if (equal? domain "us")
       #"<div class=\"amazon\" style=\"float:~|float|;\"><iframe src=\"http://rcm.amazon.com/e/cm?\
-          t=practschem-20&o=1&p=8&l=as1&asins=~|asin|&\
+          t=~|aid-us|&o=1&p=8&l=as1&asins=~|asin|&\
           fc1=000000&IS2=1&lt1=_blank&m=amazon&lc1=0000FF&\
           bc1=000000&bg1=FFFFFF&f=ifr\" style=\"width:120px;height:240px;\" \
           scrolling=\"no\" marginwidth=\"0\" marginheight=\"0\" \
           frameborder=\"0\"></iframe></div>"
       #"<div class=\"amazon\" style=\"float:~|float|;\"><iframe src=\"http://rcm-jp.amazon.co.jp/e/cm?\
           lt1=_blank&bc1=000000&IS2=1&nou=1&bg1=FFFFFF&\
-          fc1=000000&lc1=0000FF&t=,|aid|&\
+          fc1=000000&lc1=0000FF&t=~|aid|&\
           o=9&p=8&l=as1&m=amazon&f=ifr&asins=~|asin|\" \
           style=\"width:120px;height:240px;\" \
           scrolling=\"no\" marginwidth=\"0\" marginheight=\"0\" \
