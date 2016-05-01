0dcc97295687de4c93e418fa6ebc816b561f9dd8
Shiro Kawai <shiro@acm.org>
2016-05-01 00:53:49 -1000

    Adapt configure to work with both 0.9.4 and 0.9.5
    Also added package.scm for 0.9.5.

---
+++ package.scm
@@ -0,0 +1,12 @@
+(define-gauche-package "WiLiKi"
+  :version "0.7_pre1"
+  :description "WikiClone written in Scheme."
+  :require (("Gauche" (>= "0.9.4")))
+  :maintainers ("shiro@acm.org")
+  :authors ("Shiro Kawai <shiro@acm.org>")
+  :homepage "http://practical-scheme.net/wiliki/wiliki.cgi"
+  :repository "https://github.com/WiLiKi"
+  :licenses ("BSD")
+  )
+  
+  
